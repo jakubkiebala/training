7a35370c298e91386133cfcc7e1dcbfdbb8e8a4b
Jakub Kiebala <kiebala.jakub@gmail.com>
2024-07-08 21:09:23 +0200

    created get_even_numbers func

--- functions.py
+++ functions.py
@@ -5,17 +5,14 @@ def add_next_number(lst):
        This function iterates through the list and adds the value of the next element
        to each element. If it is the last element of the list, nothing is added and
        the function terminates.
+   """
 
-       Parameters:
-       lst (list): List of integers.
-
-       Returns:
-       list: List with values added from the next elements.
-
-       Example:
-       lst = [1, 3, 5, 6, 8]
-       add_next_number(lst)
-       [4, 8, 11, 14, 8]
-       """
     lst = [lst[i] + lst[i + 1] if i < len(lst) - 1 else lst[i] for i in range(len(lst))]
+    return lst
+
+
+def get_even_numbers(lst):
+    # Returns a list containing the even numbers from the input list or empty list, when it's empty.
+
+    lst = [lst[i] for i in range(len(lst)) if lst[i] % 2 == 0]
     return lst
