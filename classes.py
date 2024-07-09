f5a2c0d9f111a2983b91644ab0366dfa1ecb9e57
Jakub Kiebala <kiebala.jakub@gmail.com>
2024-07-09 10:58:29 +0200

    created calculator class with some basic operations

---
+++ classes.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
+    def raise_to_power(self, a, b):
+        return a ** b
+
+    def root(self, a, b):
+        return a ** (1 / b)
