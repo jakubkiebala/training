2e2f6af60e830c756c3cb155a828d0d27bdff1e4
Jakub Kiebala <kiebala.jakub@gmail.com>
2024-07-09 11:44:46 +0200

    created transform_words_1 func

--- functions.py
+++ functions.py
@@ -64,3 +64,44 @@ def process_numbers_1(lst):
     else:
         print('List must contains integers')
     return None
+
+
+def transform_words_1(lst):
+    """
+        Transforms a list of strings according to specified rules.
+
+        The function checks if the input list contains only strings. If so, it returns a new list where:
+        - Words longer than 5 characters are replaced by their length.
+        - Words with exactly 5 characters are replaced by the string "five".
+        - Words shorter than 5 characters are transformed to uppercase.
+        - Empty strings are omitted.
+
+        If the input list contains non-string elements, it returns a string indicating that the list must contain strings only.
+
+        Parameters:
+        lst (list): The list to process, expected to contain only strings.
+
+        Returns:
+        list or str: A new list of transformed strings or an error message if non-string elements are found.
+
+        Example:
+        >>> transform_words_1(["apple", "banana", "cat", "", "dog", "elephant"])
+        ['five', 'banana', 'CAT', 'DOG', 8]
+
+        >>> transform_words_1([1, 2, "cat", "elephant"])
+        'List must contain strings only'
+
+        Note:
+        - Words longer than 5 characters are replaced by their length.
+        - Words with exactly 5 characters are replaced by the string "five".
+        - Words shorter than 5 characters are transformed to uppercase.
+        - Empty strings are omitted.
+        """
+    is_only_str = all(isinstance(x, str) for x in lst)
+    if is_only_str:
+        lst = [f'{len(x)}' if len(x) > 5 else
+               'five' if len(x) == 5 else
+               x.upper() for x in lst if x != '']
+        return lst
+    else:
+        return 'List must contains strings only'
