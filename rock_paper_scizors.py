9c93b364dc8aac591b0ed28ef588204bc0ca3cf4
Jakub Kiebala <kiebala.jakub@gmail.com>
2025-03-31 23:16:35 +0200

    created full game logic

--- rock_paper_scizors.py
+++ rock_paper_scizors.py
@@ -2,19 +2,38 @@ from random import choice
 
 
 def computer_random_choice_func():
-    computer_choice = choice(['rock', 'paper', 'scizors'])
+    computer_choice = choice(['rock', 'paper', 'scissors'])
     return computer_choice
 
 
 def player_choice_func():
-    choices = ['rock', 'paper', 'scizors']
+    choices = ['rock', 'paper', 'scissors']
+
     while True:
         player_choice = input('write your choice : ').lower()
         if player_choice in choices:
-            print(player_choice)
             return player_choice
         else:
             print('write correct choice')
 
 
-player_choice_func()
+
+def game_logic():
+    computer_choice = computer_random_choice_func()
+    player_choice = player_choice_func()
+    print(f'Your choice : {player_choice} | Computer choice : {computer_choice}')
+
+    if player_choice == computer_choice:
+        print('Draw')
+    elif player_choice == 'rock' and computer_choice == 'scissors':
+        print('You won')
+    elif player_choice == 'scissors' and computer_choice == 'paper':        
+        print('You won')
+    elif player_choice == 'paper' and computer_choice == 'rock':
+        print('You won')
+    else:
+        print('You loose')
+
+
+
+game_logic()
