96fb89a895ba7a1dbdc37a9224ec51bc02da9b6e
Jakub Kiebala <kiebala.jakub@gmail.com>
2025-04-02 23:30:27 +0200

    created func to safe getting element of a list

--- python1.py
+++ python1.py
@@ -1,19 +1,8 @@
-import re
-import heapq
-
-def summarize_text(text, num_sentences=3):
-    sentences = re.split(r'(?<=[.!?]) +', text)
-
-    word_frequencies = {}
-    for word in re.findall(r'\w+', text.lower()):
-        word_frequencies[word] = word_frequencies.get(word, 0) + 1
-
-    sentence_scores = {}
-    for sentence in sentences:
-        for word in sentence.lower().split():
-            if word in word_frequencies:
-                sentence_scores[sentence] = sentence_scores.get(sentence, 0) + word_frequencies[word]
-
-    summary_sentences = heapq.nlargest(num_sentences, sentence_scores, key=sentence_scores.get)
-
-    return " ".join(summary_sentences)
+def safe_get(lst, index):
+    try:
+        return lst[index]
+    except TypeError:
+        print('Thats not a number')
+    except IndexError:
+        print('Bad index')
+    return None
