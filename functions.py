9081822f81e54be3c197cf45ae3b2848367d0e16
Jakub Kiebala <kiebala.jakub@gmail.com>
2025-04-04 23:09:39 +0200

    created Shape object

--- functions.py
+++ functions.py
@@ -432,3 +432,41 @@ def safe_get(lst, index):
     except IndexError:
         print('Bad index')
     return None
+
+
+class Calculator:
+    def __init__(self, name='user_calculator'):
+        self.name = name
+        self.memorized_operations = []
+
+
+    def add(self, num1, num2):
+        result = num1 + num2
+        message = f'added {num1} to {num2} got {result}'
+        self.memorized_operations.append(message)
+        print(message)
+
+    
+    def subtract(self, num1, num2):
+        result = num1 - num2
+        message = f'subtracted {num1} from {num2} got {result}'
+        self.memorized_operations.append(message)
+        print(message)
+
+
+    def multiply(self, num1, num2):
+        result = num1 * num2
+        message = f'multiplied {num1} with {num2} got {result}'
+        self.memorized_operations.append(message)
+        print(message)
+
+
+    def divide(self, num1, num2):
+        if num2 == 0:
+            print('You cant divide by zero')
+        else:
+            result = num1 / num2
+            message = f'divided {num1} by {num2} got {result}'
+            self.memorized_operations.append(message)
+            print(message)
+
--- python1.py
+++ python1.py
@@ -1,40 +1,13 @@
-class Calculator:
-    def __init__(self, name='user_calculator'):
-        self.name = name
-        self.memorized_operations = []
-
-
-    def add(self, num1, num2):
-        result = num1 + num2
-        message = f'added {num1} to {num2} got {result}'
-        self.memorized_operations.append(message)
-        print(message)
+class Shape:
+    def __init__(self, x, y, color):
+        self.color = color
+        self.center = (x, y)
+        self.description = ''
 
     
-    def subtract(self, num1, num2):
-        result = num1 - num2
-        message = f'subtracted {num1} from {num2} got {result}'
-        self.memorized_operations.append(message)
-        print(message)
+    def describe(self, description):
+        self.description = description
 
-
-    def multiply(self, num1, num2):
-        result = num1 * num2
-        message = f'multiplied {num1} with {num2} got {result}'
-        self.memorized_operations.append(message)
-        print(message)
-
-
-    def divide(self, num1, num2):
-        if num2 == 0:
-            print('You cant divide by zero')
-        else:
-            result = num1 / num2
-            message = f'divided {num1} by {num2} got {result}'
-            self.memorized_operations.append(message)
-            print(message)
-
-
-calc = Calculator()
-calc.subtract(1, 3)
-
+    def __str__(self):
+        return f'Shape with color {self.color} o środku w punktach {self.center}'
+    
