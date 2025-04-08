05efed129296e49b1302bc6daca8c6ffaba1dd35
Jakub Kiebala <kiebala.jakub@gmail.com>
2025-04-08 23:55:50 +0200

    created child class Discount20PercentCart

--- python1.py
+++ python1.py
@@ -14,3 +14,27 @@ class Cart:
     def summary(self):
         return print(f'Your cart  includes : {self.products}')
 
+
+class Discount20PercentCart(Cart):
+    def __init__(self):
+        super().__init__()
+
+    
+    def add(self, price, tag):
+        super().add(price, tag)
+
+    
+    def summary(self):
+        products_list = []
+        for product in self.products:
+            list_product = list(product)
+            tuple_product = tuple([list_product[0] * 0.8, product[1]])
+            products_list.append(tuple_product)
+        return print(f'your cart with a discount : {products_list}')
+
+
+x = Discount20PercentCart()
+x.add(9, ' ta')
+x.add(10, 'warz')
+x.add(100, 'Ogor')
+x.summary()
