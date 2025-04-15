e4f82ff457ae1a91e5dd5c4e52cc521be336c527
Jakub Kiebala <kiebala.jakub@gmail.com>
2025-04-15 23:20:50 +0200

    added method for setting up a pin number to class BankAccount

--- python1.py
+++ python1.py
@@ -2,7 +2,22 @@ class BankAccount:
     def __init__(self, owner):
         self.owner = owner
         self.balance = 0.0
-    
+        self.pin = None
+
+    def pin_set_up(self):
+        pin = []
+        for i in range (0, 4):
+            while True:
+                pin_number = input(f'Enter {i+1} number of your new pin code: ')
+                if pin_number in '0123456789':
+                    pin_number = int(pin_number)
+                    pin.append(pin_number)
+                    break
+                else:
+                    print(f'Incorrect data, try again {pin_number}')
+        self.pin = int(''.join(str(digit) for digit in pin))
+
+
     def deposit(self):
         while True:
             amount = input('Enter the number: ')
@@ -32,4 +47,9 @@ class BankAccount:
                 print('Invalid input, try again')
         
     def __str__(self):
-        return f'Welcome {self.owner}, your bank account is about |{self.balance}|'
+        return f'Welcome {self.owner}, your bank account is about |{self.balance}|, your pin : {self.pin}'
+    
+
+account = BankAccount('Kubus')
+account.pin_set_up()
+print(account)
